271807fbd4630d4f49cc1bd15a8020c7e9e14b18
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2023-12-20 05:13:42 +0000

    Maximum Subarray is done and pass on Leet Code

---
+++ MaximumSubarray/MaximumSubarray/Program.cs
@@ -0,0 +1,21 @@
+﻿//TERMINADO Y PASO EN LEET CODE
+public class Solution {
+    public int MaxSubArray(int[] nums)
+    {
+        int max = nums[0];
+        
+        for(int i = 1; i<nums.Length; i++)
+        {
+            nums[i] = Math.Max(nums[i-1]+nums[i], nums[i]);
+            max = (nums[i]>max)?nums[i]:max;
+        }
+        return max;
+    }
+    static void Main(string [] args)
+    {
+        int[] nums = new int[]{-1,-2};
+        Solution ob = new Solution();
+        ob.MaxSubArray(nums);
+    }
+
+}
