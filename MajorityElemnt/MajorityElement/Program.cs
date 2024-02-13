ff3d01c953ef0bf7b4c30693ef34ec2d42259807
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-02-13 00:39:42 +0000

    Majority Element is done and pass on LeetCode

---
+++ MajorityElemnt/MajorityElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        Array.Sort(nums);
+        return nums[nums.Length/2];
+        // var g = nums.GroupBy( i => i ).OrderByDescending(group => group.Count());
+        // var l = g.ToList();
+        // l[0].Key;
+    
+        // int deb = 0;
+       // return 0;
+ 
+    }
+    static void Main(string[] args)
+    {
+        Solution ob = new Solution();
+        ob.MajorityElement(new int[] {1,1,3,3, 1,1});
+
+    }
+}
