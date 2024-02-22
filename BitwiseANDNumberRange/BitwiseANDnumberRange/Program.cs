ffd699eb7ee38989a2455ece83742962fbc74e8e
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-02-22 01:59:58 +0000

    Bitwise AND of Numbers Range is done and pass on Leet Code

---
+++ BitwiseANDNumberRange/BitwiseANDnumberRange/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    public int RangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+        
+    }
+
+    static void Main(string[] args)
+    {
+       int[] nums = new int[]{0,0,1,-5,-6};
+       Solution ob = new Solution();
+       ob.RangeBitwiseAnd(1,2147483647);
+
+    
+    }
+}
