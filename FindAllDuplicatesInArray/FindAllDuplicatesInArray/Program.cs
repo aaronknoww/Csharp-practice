189ff12883a48cfcf065b3673b240ea21ae28209
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-03-26 00:30:28 +0000

    Find All Duplicates in an Array is done and pass on Leet Code

---
+++ FindAllDuplicatesInArray/FindAllDuplicatesInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+// TERMINADO Y PASO EN LEET CODE
+public class Solution {
+    public IList<int> FindDuplicates(int[] nums)
+    {
+        int[] l = new int[nums.Length];
+        List<int> res = new List<int>();
+        foreach (int item in nums)
+        {
+            l[item-1]++;
+            if(l[item-1]==2)
+                res.Add(item);
+        }
+        return res;
+        
+    }
+    static void Main(string[] args)
+    {
+       int[] nums = new int[]{4,3,2,7,8,2,3,1};
+       int amount = 11;
+    
+       Solution ob = new Solution();
+       ob.FindDuplicates(nums);
+      
+ 
+    }
+}
