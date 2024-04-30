b041f12b9c68785b5d09aaf9d4c662a3e7008373
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-04-30 00:29:27 +0000

    Print First n Fibonacci Numbers is done and pass on GFG

---
+++ FirstNthFiboNumber/FirstNthFiboNumber/Program.cs
@@ -0,0 +1,60 @@
+﻿//{ Driver Code Starts
+//Initial Template for C#
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverCode
+{
+
+    class GFG
+    {
+        static void Main(string[] args)
+        {
+            int testcases;// Taking testcase as input
+            testcases = Convert.ToInt32(Console.ReadLine());
+            while (testcases-- > 0)// Looping through all testcases
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+                Solution obj = new Solution();
+                List<long> res = obj.printFibb(N);
+                foreach (long i in res)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for C#
+
+// TERMINADO Y PASO EN GFG
+class Solution
+{
+    //Complete this function
+    public List<long> printFibb(int n)
+    {
+        if(n == 1)
+            return new List<long>(){1};
+        if(n == 2 )
+            return new List<long>(){1,1};
+        List<long> res = new List<long>();
+        res.Add(1);
+        res.Add(1);
+        for (int i = 2; i < n; i++)
+        {
+            res.Add(res[i-1] + res[i-2]);
+        }
+
+        return res;
+        //Your code here
+    }
+}
