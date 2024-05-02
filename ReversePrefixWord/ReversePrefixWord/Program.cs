5e93ab6cdb5c2a92be0b1c433f5a2db5968196f3
aaronknoww <58243426+aaronknoww@users.noreply.github.com>
2024-05-02 02:21:28 +0000

    Reverse Prefix Word is done and pass on Leet Code

---
+++ ReversePrefixWord/ReversePrefixWord/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Solution {
+    public string ReversePrefix(string word, char ch)
+    {
+        //List<char> w = new List<char>();
+        //w = word.ToList();
+        string w = new string(word);
+        string inv = "";
+        int i = 0;
+        foreach (char c in w)
+        {
+            if(c == ch)
+            {
+                inv = c + inv;
+                i++;
+                w = w.Remove(0, i);
+                return inv + w.ToString();
+            } 
+            inv = c + inv;
+            i++;
+            
+        }
+        return word;
+    }
+
+    static void Main(string[] args)
+    {   
+
+        string w = "abcdefd";
+        char c = 'd';
+       int[] nums = new int[]{-4,-1,0,3,10};
+       
+       //return res;
+
+
+      Solution ob = new Solution();
+       ob.ReversePrefix(w,c);
+
+    
+    }
+}
